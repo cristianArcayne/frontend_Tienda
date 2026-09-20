0ed0519512bd64e854739a92572748a71dd910df
Cristian <cmamani096@gmail.com>
2026-09-20 00:45:00 -0400

    fix: apuntar environment y angular.json a render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  isMultitenant: true, // true = cada subdominio es una empresa
-  apiBaseUrl: 'http://127.0.0.1:8000/api', // Fallback si no está en subdominio
+  isMultitenant: false,
+  apiBaseUrl: 'https://tienda-backend-kvfk.onrender.com/api',
 };
