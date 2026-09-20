e0faf0140081ef0bcd48863e1f0bbf493a272801
Cristian <cmamani096@gmail.com>
2026-09-20 01:23:52 -0400

    chore: environment local 127.0.0.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   isMultitenant: false,
-  apiBaseUrl: 'https://tienda-backend-kvfk.onrender.com/api',
+  apiBaseUrl: 'http://127.0.0.1:8000/api',
 };
